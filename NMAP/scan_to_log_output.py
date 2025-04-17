ce79f8c67955c46c58ee555e2854275650949d09
tombyles <tomgbyles@gmail.com>
2025-04-17 13:26:43 -0600

    Update scan_to_log_output.py
    updated UDP scan

--- NMAP/scan_to_log_output.py
+++ NMAP/scan_to_log_output.py
@@ -6,6 +6,7 @@ TARGET = "192.168.4.0/24"  # Your network range
 NMAP_SCRIPT = "./iot-vuln-check_updated.nse"  # path to your NSE script
 NMAP_PORTS = "21,22,23,80,443,554,1883,8883,1900,8000,8080,8443,8888,49152-49157,6666,6667,5353,5000"
 LOG_OUTPUT = "scan_results.log"  # Save Nmap log output only
+TIMEOUT = 600
 
 # === Step 1: Run Nmap with your custom NSE script ===
 def run_nmap():
@@ -13,14 +14,18 @@ def run_nmap():
     try:
         subprocess.run([
             "nmap",
+            "-sS", "-sU",
             "-p", NMAP_PORTS,
             "--script", NMAP_SCRIPT,
             "-oN", LOG_OUTPUT,  # Save the results only to log file
             "-Pn",  # Skip host discovery (treat all hosts as online)
             "-T4",  # Faster scan
             TARGET
-        ], check=True)
-        print("[+] Nmap scan complete.Results saved to", LOG_OUTPUT)
+        ], check=True, timeout=TIMEOUT)
+        print("[+] Nmap scan complete. Results saved to", LOG_OUTPUT)
+    except subprocess.TimeoutExpired:
+        print(f"[!] Nmap scan timed out after {TIMEOUT} seconds.")
+        exit(1)
     except subprocess.CalledProcessError as e:
         print("[!] Nmap scan failed:", e)
         exit(1)
