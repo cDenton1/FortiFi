bf6734fcb228139d32a7705bad7d4a03e3447ac2
tombyles <tomgbyles@gmail.com>
2025-04-17 11:43:03 -0600

    Update scan_to_log_output.py
    should be fixed

--- NMAP/scan_to_log_output.py
+++ NMAP/scan_to_log_output.py
@@ -3,7 +3,7 @@ import os
 
 # === CONFIG ===
 TARGET = "192.168.4.0/24"  # Your network range
-NMAP_SCRIPT = "./iot-vuln-check.nse"  # path to your NSE script
+NMAP_SCRIPT = "./iot-vuln-check_updated.nse"  # path to your NSE script
 NMAP_PORTS = "21,22,23,80,443,554,1900"
 LOG_OUTPUT = "scan_results.log"  # Save Nmap log output only
 
@@ -18,7 +18,7 @@ def run_nmap():
             "-oN", LOG_OUTPUT,  # Save the results only to log file
             TARGET
         ], check=True)
-        print("[+] Nmap scan complete.")
+        print("[+] Nmap scan complete.Results saved to", LOG_OUTPUT)
     except subprocess.CalledProcessError as e:
         print("[!] Nmap scan failed:", e)
         exit(1)
